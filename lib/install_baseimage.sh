a3cd23829b87ff108e15bbe36986fb612cc57368
Nordic Energy Dev <support@nordicenergy.io>
2019-12-24 05:48:25 +0000

    Add README.md

---
+++ lib/install_baseimage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#This is for base tag of image syneblock/quorum-master:base
+apt-get update
+apt-get install -y libdb-dev libleveldb-dev libsodium-dev zlib1g-dev libtinfo-dev
+apt-get install -y build-essential
+apt-get install software-properties-common python-software-properties -y
+apt-get install -y wget
+apt-get install -y curl
+apt-get install -y jq
+apt-get install -y psmisc
+apt-get install -y iputils-ping
+add-apt-repository ppa:ethereum/ethereum -y
+apt-get update
+apt-get install solc -y
+apt-get install bsdmainutils
+apt-get install -y openjdk-8-jdk
+apt-get -y install maven
+apt-get -y install net-tools
